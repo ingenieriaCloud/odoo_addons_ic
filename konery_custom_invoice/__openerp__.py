aca57cf09c2f2ceeac2d90b40a6cd22e64712b00
Francisco Garcia Claramonte <francisco@garciac.es>
2016-02-01 18:08:20 +0100

    Ajsutes en formatos de tablas

--- konery_custom_invoice/__openerp__.py
+++ konery_custom_invoice/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
    'name': 'Personalización de informe de Factura',
-   'version': '8.0.1.1.0',
+   'version': '8.0.1.1.10',
    'author': 'Ingeniería Cloud',
    'category': 'Ingeniería Cloud',
    'depends': [
--- konery_custom_order/__openerp__.py
+++ konery_custom_order/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
    'name': 'Personalización de informe de Presupuestos de Konery',
-   'version': '8.0.1.1.4',
+   'version': '8.0.1.1.5',
    'author': 'Ingeniería Cloud',
    'category': 'Ingeniería Cloud',
    'depends': [
