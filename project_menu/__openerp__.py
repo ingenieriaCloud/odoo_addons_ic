4c4c9f7f62c22d8edb59ef350c10e7bbbdb72f56
Francisco Garcia Claramonte <francisco@garciac.es>
2016-02-11 19:27:59 +0100

    Menús para proyectos

---
+++ project_menu/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    project_menu
+#    Copyright (c) 2016 Francisco Manuel García Claramonte <francisco@garciac.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+   'name': 'Project and tasks direct menu',
+   'version': '8.0.0.0.5',
+   'author': 'Ingeniería Cloud',
+   'category': 'Ingeniería Cloud',
+   'depends': [
+       'crm',
+   ],
+   'website': 'https://www.serincloud.org',
+   'description': """
+Module to create direct menus access
+====================================   
+
+ Menu to projects and tasks
+ 
+  
+  Copyright (c) 2015-2016 Ingeniería Cloud
+
+  Copyright (c) 2015-2016 Francisco Manuel García Claramonte
+
+
+   """,
+   'data': [
+       'menu/project_menu.xml',
+   ],
+}
