dae1c79a16b1e3eecd07fcd7d085ea25edcdd6f1
Francisco Garcia Claramonte <francisco@garciac.es>
2016-03-04 18:38:11 +0100

    Nuevo ajuste. Ocultado n.cuenta cliente

--- konery_custom_invoice/__openerp__.py
+++ konery_custom_invoice/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
    'name': 'Personalización de informe de Factura',
-   'version': '8.0.2.0.16',
+   'version': '8.0.2.0.17',
    'author': 'Ingeniería Cloud',
    'category': 'Ingeniería Cloud',
    'depends': [
