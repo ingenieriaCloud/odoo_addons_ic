0b0c55ec7ffaf2692eaecf88d4574fff2cc6c35b
francisco <francisco@debian>
2016-04-21 18:05:25 +0200

    Modificada creación de Transportistas y producto de trazabilidad

--- reinicia_custom/__openerp__.py
+++ reinicia_custom/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
    'name': 'Personalización de gestión para Reinicia',
-   'version': '8.0.0.1.11',
+   'version': '8.0.0.1.14',
    'author': 'Ingeniería Cloud',
    'category': 'Ingeniería Cloud',
    'depends': [
