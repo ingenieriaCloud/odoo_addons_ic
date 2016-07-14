0782cc50fe27b551d0d884aace516d7369d038d7
francisco <fgclaramonte@gmail.com>
2016-07-14 17:45:11 +0200

    WIP: Módulo para actualizar precio de productos

---
+++ product_update_price_wizard/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    product_update_price_wizard
+#    Copyright (c) 2016 Francisco Manuel García Claramonte <francisco@garciac.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Update product price from pricelist',
+    'version': '8.0.0.0.1',
+    'category': 'Product',
+    'description': """
+Module for update product price
+===============================
+
+
+Publicado bajo licencia AGPL-v3.
+
+Copyright (c) 2016 Ingeniería Cloud
+
+Copyright (c) 2016 Francisco Manuel García Claramonte
+
+    """,
+    'author': 'Francisco M. García Claramonte',
+    'website': 'http://www.garciac.es',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_update_price_wizard_view.xml',
+    ],
+    "installable": True,
+}
---
+++ product_update_price_wizard/product_update_price_wizard.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    product_update_price_wizard
+#    Copyright (c) 2016 Francisco Manuel García Claramonte <francisco@garciac.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+from openerp import exceptions
+
+class ProductUpdatePriceWizard(models.TransientModel):
+    _name = 'product.update_price_wizard'
+    _description = 'Update product price'
+
+    pricelist = fields.Many2one('product.pricelist', string="Tarifa")
+
+    @api.multi
+    def do_mass_update(self):
+        self.ensure_one()
+        if not (self.pricelist):
+            raise exceptions.ValidationError('Debe seleccionar una tarifa')
+
+        prods_obj = self.env['product.template']
+        all_prods = prods_obj.browse(self.env.context['active_ids'])
+        
+        pricelist = self.env['product.pricelist'].browse(self.pricelist.id)
+
+        for p in all_prods:
+            precio = pricelist.price_get(prod_id=p.id, qty=1.0)
+            #import pudb; pudb.set_trace()
+            p.write({'list_price': precio[1]})
+
